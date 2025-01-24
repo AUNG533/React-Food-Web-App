3872435806338c68b7018c89630d805dbd096150
hongs <aungkyawlinmon@gmail.com>
2025-01-24 19:34:13 +0700

    Header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id dolorem
+          atque sed, hic inventore optio exercitationem corrupti quidem non
+          eligendi, fugit rem, at ducimus nihil fugiat? Nihil quam reiciendis
+          odio!
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
