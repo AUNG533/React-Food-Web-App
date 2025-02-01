00ce04c9af03d59c2ea2fb65ac1937fa01ae69a7
hongs <aungkyawlinmon@gmail.com>
2025-02-01 10:33:23 +0700

    sidebar and add product

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { Routes, Route } from 'react-router-dom'
+import Add from "./pages/Add/Add";
+import List from "./pages/List/List";
+import Orders from "./pages/Orders/Orders";
+import { ToastContainer } from 'react-toastify';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hello world</h1>
+    <ToastContainer/>
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/list" element={<List/>}/>
+          <Route path="/orders" element={<Orders/>}/>
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
