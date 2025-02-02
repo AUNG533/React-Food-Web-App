406efef22961a2b864b7f8ca7b77607bc21c9174
hongs <aungkyawlinmon@gmail.com>
2025-02-02 20:20:47 +0700

    Food List

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -92,6 +92,7 @@ const Add = () => {
               <option value="Rolls">Rolls</option>
               <option value="Deserts">Deserts</option>
               <option value="Sandwich">Sandwich</option>
+              <option value="Cake">Cake</option>
               <option value="Pure Veg">Pure Veg</option>
               <option value="Pasta">Pasta</option>
               <option value="Noodles">Noodles</option>
